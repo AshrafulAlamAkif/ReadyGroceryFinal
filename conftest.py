0a1288425f8b4574ebe7b44cf2d6e4f4d3858025
Ashraful Alam Akif <ashraful15-2225@diu.edu.bd>
2025-10-30 18:27:23 +0600

    Base url setup to New way

--- conftest.py
+++ conftest.py
@@ -1,8 +1,18 @@
+# ---------- ✅ common setup/teardown থাকবে ----------
 import pytest
 from selenium import webdriver
-# from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+
+'''
+# ---------- BASE URL ----------
+base_url = "https://uat-readygrocery.razinsoft.com/admin/login"
+@pytest.fixture(scope="session")
+def base_url():
+    """Return base URL for all tests"""
+    return base_url
+'''
+                    #or
 
 # ✅ Base URL Fixture
 @pytest.fixture(scope="session")
